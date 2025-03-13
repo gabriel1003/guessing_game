3eb09e92a658a1022dd8ab44c45b8c85b7917a50
gabriel1003 <gabriel.samersla97@gmail.com>
2025-03-13 16:07:57 -0300

    feat: implementa um laco de repeticao permitindo assim que o usuario possa ter mais de uma tentativa

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("O número secreto é: {secret_number}");
-    
-    println!("Digite o seu palpite: ");
+    loop {
+        println!("Digite o seu palpite: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-    .read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+        .read_line(&mut guess).expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
-    println!("Você digitou: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Muito pequeno!"),
-        Ordering::Greater => println!("Muito grande!"),
-        Ordering::Equal => println!("Você ganhou!"),
+        println!("Você digitou: {}", guess);
 
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Muito pequeno!"),
+            Ordering::Greater => println!("Muito grande!"),
+            Ordering::Equal => {
+                println!("Você ganhou!");
+break;
+            }
+        }
     }
 }
