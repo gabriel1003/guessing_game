7761927c8dde1b6c9390f49ecaf12d7b2e15f453
gabriel1003 <gabriel.samersla97@gmail.com>
2025-03-13 14:45:59 -0300

    feat: implementamos abiblioteca rand para gerar o numero aleatorio que sera adivinhado

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Adivinhe o número!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("O número secreto é: {secret_number}");
+    
     println!("Digite o seu palpite: ");
 
     let mut guess = String::new();
